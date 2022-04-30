09f48b91686cf2f292d131524335fb4330b010ea
4eDo <61490588+4eDo@users.noreply.github.com>
2022-04-30 16:55:31 +0300

    Update skillslist.js

--- skillslist.js
+++ skillslist.js
@@ -3,28 +3,24 @@
 		"id": 11,
 		"type": "Бытовой",
 		"level": "низкий",
-		"classColour": "green1",
 		"style": "color: #034000;"
 	},
 	{
 		"id": 12,
 		"type": "Бытовой",
 		"level": "средний",
-		"classColour": "green2",
 		"style": "color: #056b00;"
 	},
 	{
 		"id": 13,
 		"type": "Бытовой",
 		"level": "высокий",
-		"classColour": "green3",
 		"style": "color: #089c00;"
 	},
 	{
 		"id": 14,
 		"type": "Бытовой",
 		"level": "наивысший",
-		"classColour": "green4",
 		"style": "color: #1bc700;"
 	},
 	
@@ -32,28 +28,24 @@
 		"id": 21,
 		"type": "Магический",
 		"level": "низкий",
-		"classColour": "violet1",
 		"style": "color: #306;"
 	},
 	{
 		"id": 22,
 		"type": "Магический",
 		"level": "средний",
-		"classColour": "violet2",
 		"style": "color: #609;"
 	},
 	{
 		"id": 23,
 		"type": "Магический",
 		"level": "высокий",
-		"classColour": "violet3",
 		"style": "color: #60f;"
 	},
 	{
 		"id": 24,
 		"type": "Магический",
 		"level": "наивысший",
-		"classColour": "violet4",
 		"style": "color: #93f;"
 	},
 	
@@ -61,28 +53,24 @@
 		"id": 31,
 		"type": "Боевой",
 		"level": "низкий",
-		"classColour": "red1",
 		"style": "color: #600;"
 	},
 	{
 		"id": 32,
 		"type": "Боевой",
 		"level": "средний",
-		"classColour": "red2",
 		"style": "color: #900;"
 	},
 	{
 		"id": 33,
 		"type": "Боевой",
 		"level": "высокий",
-		"classColour": "red3",
 		"style": "color: #c00;"
 	},
 	{
 		"id": 34,
 		"type": "Боевой",
 		"level": "наивысший",
-		"classColour": "red4",
 		"style": "color: #f00;"
 	}
 ]
