f87395842f7b9f71b1f07399627b90dec463312f
4eDo <61490588+4eDo@users.noreply.github.com>
2022-04-30 18:47:57 +0300

    Create am_scrolls_datalist.js

---
+++ am_scrolls_datalist.js
@@ -0,0 +1,188 @@
+[
+  {
+    "id": 11,
+    "name": "Щит",
+    "bb": "[svshield]",
+    "style": "color: #1a45c1;"
+  },
+  {
+    "id": 12,
+    "name": "Меч",
+    "bb": "[svmech]",
+    "style": "color: #1a45c1;"
+  },
+  {
+    "id": 13,
+    "name": "Щит от магии",
+    "bb": "[svmagshield]",
+    "style": "color: #1a45c1;"
+  },
+  {
+    "id": 14,
+    "name": "Магический шар",
+    "bb": "[svmagshar]",
+    "style": "color: #1a45c1;"
+  },
+  {
+    "id": 15,
+    "name": "Портал",
+    "bb": "[svportal]",
+    "style": "color: #1a45c1;"
+  },
+  {
+    "id": 21,
+    "name": "Бытовой навык",
+    "bb": "[svbitnav]",
+    "style": "color: #cf682d;"
+  },
+  {
+    "id": 22,
+    "name": "Навык опыта",
+    "bb": "[svmbnav]",
+    "style": "color: #cf682d;"
+  },
+  {
+    "id": 31,
+    "name": "Обмен 1 МБО",
+    "bb": "[mbo_change_1]",
+    "style": "color: #1f8569;"
+  },
+  {
+    "id": 32,
+    "name": "Обмен 5 МБО",
+    "bb": "[mbo_change_5]",
+    "style": "color: #1f8569;"
+  },
+  {
+    "id": 33,
+    "name": "Обмен 20 МБО",
+    "bb": "[mbo_change_20]",
+    "style": "color: #1f8569;"
+  },
+  {
+    "id": 401,
+    "name": "1 ИО",
+    "bb": "[svio1]",
+    "style": "color: #6e5642;"
+  },
+  {
+    "id": 402,
+    "name": "2 ИО",
+    "bb": "[svio2]",
+    "style": "color: #6e5642;"
+  },
+  {
+    "id": 403,
+    "name": "3 ИО",
+    "bb": "[svio3]",
+    "style": "color: #6e5642;"
+  },
+  {
+    "id": 404,
+    "name": "4 ИО",
+    "bb": "[svio4]",
+    "style": "color: #6e5642;"
+  },
+  {
+    "id": 405,
+    "name": "5 ИО",
+    "bb": "[svio5]",
+    "style": "color: #6e5642;"
+  },
+  {
+    "id": 406,
+    "name": "6 ИО",
+    "bb": "[svio6]",
+    "style": "color: #6e5642;"
+  },
+  {
+    "id": 407,
+    "name": "7 ИО",
+    "bb": "[svio7]",
+    "style": "color: #6e5642;"
+  },
+  {
+    "id": 408,
+    "name": "8 ИО",
+    "bb": "[svio8]",
+    "style": "color: #6e5642;"
+  },
+  {
+    "id": 409,
+    "name": "9 ИО",
+    "bb": "[svio9]",
+    "style": "color: #6e5642;"
+  },
+  {
+    "id": 410,
+    "name": "10 ИО",
+    "bb": "[svio10]",
+    "style": "color: #6e5642;"
+  },
+  {
+    "id": 411,
+    "name": "15 ИО",
+    "bb": "[svio15]",
+    "style": "color: #6e5642;"
+  },
+  {
+    "id": 412,
+    "name": "20 ИО",
+    "bb": "[svio20]",
+    "style": "color: #6e5642;"
+  },
+  {
+    "id": 413,
+    "name": "30 ИО",
+    "bb": "[svio30]",
+    "style": "color: #6e5642;"
+  },
+  {
+    "id": 501,
+    "name": "1 ОО",
+    "bb": "[svmbo1]",
+    "style": "color: #ff0000;"
+  },
+  {
+    "id": 502,
+    "name": "2 ОО",
+    "bb": "[svmbo2]",
+    "style": "color: #ff0000;"
+  },
+  {
+    "id": 503,
+    "name": "3 ОО",
+    "bb": "[svmbo3]",
+    "style": "color: #ff0000;"
+  },
+  {
+    "id": 504,
+    "name": "4 ОО",
+    "bb": "[svmbo4]",
+    "style": "color: #ff0000;"
+  },
+  {
+    "id": 505,
+    "name": "5 ОО",
+    "bb": "[svmbo5]",
+    "style": "color: #ff0000;"
+  },
+  {
+    "id": 506,
+    "name": "6 ОО",
+    "bb": "[svmbo6]",
+    "style": "color: #ff0000;"
+  },
+  {
+    "id": 507,
+    "name": "7 ОО",
+    "bb": "[svmbo7]",
+    "style": "color: #ff0000;"
+  },
+  {
+    "id": 508,
+    "name": "10 ОО",
+    "bb": "[svmbo10]",
+    "style": "color: #ff0000;"
+  }
+]
