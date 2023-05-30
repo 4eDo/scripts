44de6e992c072c31704826d7caee03694fb23e00
4eDo <61490588+4eDo@users.noreply.github.com>
2023-05-30 12:12:23 +0300

    Update lazyinit.js

--- lazyinit.js
+++ lazyinit.js
@@ -1,5 +1,5 @@
     $(document).ready(function() { 
-        if(!$("body > main > div > h2:contains('edit')").length == 0){ 
+        if(!$("body > main > div > h2:contains('edit')").length == 0 && document.getElementById('postamattable')){ 
             document.querySelector("#partnertable > tbody > tr:nth-child(2)").setAttribute("hidden", true);
             document.querySelector("#partnertable > tbody > tr:nth-child(2) > td:nth-child(2) > p > input[type=text]").setAttribute("readonly", true);
             document.querySelector("body > main > div > h2").innerText = document.querySelector("#partnertable > tbody > tr:nth-child(2) > td:nth-child(2) > p > input[type=text]").value;
