cb26b015d8f4617f3339e17182818776d71470d1
4eDo <61490588+4eDo@users.noreply.github.com>
2023-05-30 11:41:40 +0300

    Update lazyinit.js

--- lazyinit.js
+++ lazyinit.js
@@ -1,7 +1,3 @@
-jQuery.noConflict()(
     $(document).ready(function() { 
-
-       if(!$("body > main > div > h2:contains('edit')").length == 0) document.querySelector("#partnertable > tbody > tr:nth-child(2)").setAttribute("hidden", true);
-
+        if(!$("body > main > div > h2:contains('edit')").length == 0) document.querySelector("#partnertable > tbody > tr:nth-child(2)").setAttribute("hidden", true);
     });
-);
