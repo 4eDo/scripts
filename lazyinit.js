3edf80133d463d1b824c6b2fbcf9f58f995ceaa7
4eDo <61490588+4eDo@users.noreply.github.com>
2023-05-30 11:31:45 +0300

    Update lazyinit.js

--- lazyinit.js
+++ lazyinit.js
@@ -1,1 +1,3 @@
-if(!$("body > main > div > h2:contains('edit')").length == 0) document.querySelector("#partnertable > tbody > tr:nth-child(2)").setAttribute("hidden", true);
+$(window).load(function() {
+  if(!$("body > main > div > h2:contains('edit')").length == 0) document.querySelector("#partnertable > tbody > tr:nth-child(2)").setAttribute("hidden", true);
+});
