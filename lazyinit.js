50a13cf5c522d8b0645092e64de7c8b6ef7e76bc
4eDo <61490588+4eDo@users.noreply.github.com>
2023-05-30 11:29:35 +0300

    Update lazyinit.js

--- lazyinit.js
+++ lazyinit.js
@@ -1,1 +1,1 @@
-// console.log("lazy init js run");
+if(!$("body > main > div > h2:contains('edit')").length == 0) document.querySelector("#partnertable > tbody > tr:nth-child(2)").setAttribute("hidden", true);
